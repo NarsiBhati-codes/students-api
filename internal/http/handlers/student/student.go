3da0f4249342651627468e9018a3ca3230231cce
NarsiBhatii <narsibhati31102@gmail.com>
2024-12-05 03:17:52 +0530

    setup database and create student functionally

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -5,16 +5,19 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"log/slog"
 	"net/http"
 
+	"github.com/NarsiBhati-codes/students-api/internal/storage"
 	"github.com/NarsiBhati-codes/students-api/internal/types"
 	response "github.com/NarsiBhati-codes/students-api/internal/utils"
 	"github.com/go-playground/validator/v10"
 )
 
 
-func New() http.HandlerFunc {
+func New(storage storage.Storage) http.HandlerFunc {
 	return func(res http.ResponseWriter, req *http.Request) {
+		slog.Info("creating a student")
 
 		var student types.Student 
 
@@ -40,6 +43,20 @@ func New() http.HandlerFunc {
 			return
 		}
 
-		response.WriteJson(res, http.StatusCreated, map[string]string {"success" : "ok"})
+		lastId, err := storage.CreateStudent(
+			student.Name,
+			student.Email,
+			student.Age,
+		)
+
+		slog.Info("user created successfully", slog.String("userId",fmt.Sprint(lastId)))
+
+
+		if err != nil {
+			response.WriteJson(res, http.StatusInternalServerError,err)
+			return
+		}
+
+		response.WriteJson(res, http.StatusCreated, map[string]int64{"id": lastId})
 	}
 }
